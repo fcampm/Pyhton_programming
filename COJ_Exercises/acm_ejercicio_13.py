8f26c20e2b6b83b40a3ef7aaa025f7f81f14ccc7
fcampm <fabian.camp@yahoo.com>
2017-03-03 17:55:03 -0600

    Exercise 13 ACM type

---
+++ COJ_Exercises/acm_ejercicio_13.py
@@ -0,0 +1,12 @@
+#  -*- coding: utf-8 -*-
+# Authors:
+#          Fabián Camp Mussa
+#
+#
+# Tema: Problem 1484.
+# Fecha: March 4, 2017.
+
+from __future__ import print_function
+from __future__ import division
+
+from sys import stdin
