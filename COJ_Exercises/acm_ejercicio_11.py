a59a0a9b47242df8d5157755477394b27c3bfc9a
fcampm <fabian.camp@yahoo.com>
2017-03-03 16:44:34 -0600

    New exercise, python_ejercicio_11.py

---
+++ COJ_Exercises/acm_ejercicio_11.py
@@ -0,0 +1,16 @@
+#  -*- coding: utf-8 -*-
+# Authors:
+#          Fabián Camp Mussa
+#
+#
+# Tema: Problema 1293
+# Fecha: 
+
+from __future__ import print_function
+from __future__ import division
+
+from sys import stdin
+
+n = int(stdin.read())
+
+print(2**n)
