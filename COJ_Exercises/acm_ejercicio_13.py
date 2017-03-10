63968f6befc0a22526b8804976427e31e558c24c
fcampm <fabian.camp@yahoo.com>
2017-03-10 16:56:54 -0600

    Program updated

--- COJ_Exercises/acm_ejercicio_13.py
+++ COJ_Exercises/acm_ejercicio_13.py
@@ -10,3 +10,17 @@ from __future__ import print_function
 from __future__ import division
 
 from sys import stdin
+
+entrada = stdin.read().split()
+
+N = int(entrada[0])
+nums = iter([float(x) for x in entrada[1:]])
+
+mayor = next(nums)
+indice = 1
+
+for x, i in zip(nums, range(2, N + 1)):
+    if x >= mayor:
+        mayor = x
+        indice = i
+print(indice)
