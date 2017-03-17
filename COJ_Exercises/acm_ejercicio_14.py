e598f7e94184fd9c0c80e04c86b10976e5d532e3
fcampm <fabian.camp@yahoo.com>
2017-03-17 16:59:41 -0600

    Program has been modified and input has been uploaded

--- COJ_Exercises/acm_ejercicio_14.py
+++ COJ_Exercises/acm_ejercicio_14.py
@@ -11,3 +11,24 @@ from __future__ import division
 
 from sys import stdin
 
+nums = iter([int(x) for x in stdin.read().split()])
+n = next(nums)
+si = set() # la función set te crea un conjunto en donde se añadirán un elemento pero el set es más eficiente que una lista 
+no = set() 
+
+for i in range(n):    
+    inicio = next(nums)
+    fin = next(nums)
+    r = []
+    for j in range(inicio, fin + 1):
+        if j in si:
+            r.append(str(j))
+        elif j not in no:
+            b = bin(j)[2:]
+            if b == b[::-1]:
+                r.append(str(j))
+                si.add(j)
+            else:
+                no.add(j)
+                
+    print (' '.join(r))
