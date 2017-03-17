b5e49d9bc0910eb733b6a0a5c108189904c6fff5
fcampm <fabian.camp@yahoo.com>
2017-03-17 15:56:09 -0600

    New program COJ type

---
+++ COJ_Exercises/acm_ejercicio_14.py
@@ -0,0 +1,13 @@
+#  -*- coding: utf-8 -*-
+# Authors:
+#          Fabián Camp Mussa
+#
+#
+# Tema: Problem 1101
+# Fecha: March 17, 2017.
+
+from __future__ import print_function
+from __future__ import division
+
+from sys import stdin
+
